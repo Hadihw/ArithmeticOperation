86cb5910a06934421d1c975359cde1d4bb1a4ded
Hadi Hawi <44330974+Hadihw@users.noreply.github.com>
2023-04-09 14:19:09 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -15,7 +15,7 @@ public class ArithmeticOperationsTest {
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
 // 		assertEquals(expected, actual);
-		fail("must fail");
+		assertTrue("This test must fail", false);
 
 	}
 //     @Test
